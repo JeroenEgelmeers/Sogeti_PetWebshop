7e0147872d083501a61ae4339aad94e738822d58
Hofstede <benjamin.hofstede@sogeti.com>
2016-11-01 15:09:40 +0100

    added jeroen's frontend

---
+++ PetSupplies2/src/main/webapp/assets/js/routeProvider.js
@@ -0,0 +1,24 @@
+/**
+ * Created by JEGELMEE on 31-10-2016.
+ */
+// Router
+angular.module('webshopApp')
+    .config(function($routeProvider) {
+        $routeProvider
+            .when("/", {
+                redirectTo: '/home'
+            })
+            .when("/home", {
+                templateUrl : "pages/home.html",
+            })
+            .when("/assortiment", {
+                templateUrl : "pages/assortiment.html",
+            })
+            .when("/contact", {
+                templateUrl : "pages/contact.html",
+            })
+            .when("/cart", {
+                templateUrl : "pages/cart.html",
+                //controller: "MainController",
+            });
+    });
